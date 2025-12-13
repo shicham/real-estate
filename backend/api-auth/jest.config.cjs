5117512e5b17e47063d182bdc831c2fa24fb0983
shicham <sass.hicham@gmail.com>
2025-12-13 09:36:00 +0100

    fix import

--- backend/api-auth/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: '.',
-  testMatch: ['<rootDir>/test/**/*.test.ts', '<rootDir>/test/**/*.test.tsx'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  // collectCoverage: true,
-  // coverageDirectory: '<rootDir>/coverage'
-}
